9b988a9206ddc2f9c34721a22e982fffb5cef0a1
Meg4Bit <pe4enko@DESKTOP-OMDU8FP.localdomain>
2023-05-28 15:34:52 +0300

    Script for autotest

--- mutmut-check.sh
+++ mutmut-check.sh
@@ -1,12 +1,27 @@
-export PYTHONPATH=$2:/home/pe4enko/Repos/mutmut
+#!/bin/bash
 
-cd $1
+export PYTHONPATH=$PYTHONPATH:$MUTMUT
 
+thresholds=(80 60 40 20 0)
 
-thresholds=(50 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400)
+git stash && git checkout master
+git checkout HEAD~100
+echo "First revision"
+time python -m mutmut run --paths-to-mutate=$1 --paths-to-exclude=$4 --use-coverage -n $3 --runner "$2"
+mv .coverage .coverage_old
 
 for t in ${thresholds[@]}; do
-  echo "Threshold is $t"
-  rm -rf .mutmut-cache
-  python -m mutmut run --paths-to-mutate=$2 --use-coverage -n $t | tail -n 1
+  git stash && git checkout master
+  git checkout HEAD~$t
+  echo "Modified mutmut"
+  time python -m mutmut run --paths-to-mutate=$1 --paths-to-exclude=$4 --use-coverage -n $3 --runner "$2"
+  mv .coverage .coverage_old
+  mv .mutmut-cache .mutmut-cache_old
+  echo "Original mutmut"
+  git stash
+  time python -m mutmut run --paths-to-mutate=$1 --paths-to-exclude=$4 --use-coverage -n $3 --runner "$2"
+  rm -rf .mutmut-cache .coverage
+  mv .mutmut-cache_old .mutmut-cache
 done
+
+git stash && git checkout master
