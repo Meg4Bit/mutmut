fd6dfa7f26cfbaa6a59b36cf0992ee46dee6e8ed
Meg4Bit <pe4enko@DESKTOP-OMDU8FP.localdomain>
2023-05-30 23:09:49 +0300

    Script for experiment

---
+++ flask-check.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export PYTHONPATH=$PYTHONPATH:$MUTMUT
+
+thresholds=(80 60 40 20 0)
+
+git stash && git checkout main
+git checkout HEAD~100
+echo "First revision"
+time python -m mutmut run --paths-to-mutate=$1 --paths-to-exclude=$4 --use-coverage -n $3 --runner "$2"
+mv .coverage .coverage_old
+
+for t in ${thresholds[@]}; do
+  git stash && git checkout main
+  git checkout HEAD~$t
+  pip-autoremove Flask -y
+  pip install . && pip install -r requirements/dev.txt
+  echo "Modified mutmut"
+  time python -m mutmut run --paths-to-mutate=$1 --paths-to-exclude=$4 --use-coverage -n $3 --runner "$2"
+  mv .coverage .coverage_old
+  mv .mutmut-cache .mutmut-cache_old
+  echo "Original mutmut"
+  git stash
+  time python -m mutmut run --paths-to-mutate=$1 --paths-to-exclude=$4 --use-coverage -n $3 --runner "$2"
+  rm -rf .mutmut-cache .coverage
+  mv .mutmut-cache_old .mutmut-cache
+done
+
+git stash && git checkout main
